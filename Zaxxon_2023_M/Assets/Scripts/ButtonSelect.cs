c62528b498a61cf53e2577e23657cc6af90977e3
Alvaro H <correo@laultimapregunta.com>
2024-02-28 09:36:28 +0100

    Bakeo luces prefab + Sonido en botones

---
+++ Zaxxon_2023_M/Assets/Scripts/ButtonSelect.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSelect : MonoBehaviour, ISelectHandler// required interface when using the OnSelect method.
+{
+
+    //Arrastramos el Audio Source que contiene el sonido que queremos ejecutar
+    AudioSource m_Source;
+
+    private void Start()
+    {
+        m_Source = GameObject.Find("Canvas").GetComponent<AudioSource>();
+    }
+    //Do this when the selectable UI object is selected.
+    public void OnSelect(BaseEventData eventData)
+    {
+        m_Source.Play();
+    }
+
+
+}
